b21238f3aaa7ef41e85206123b88c92a0daeb83b
Andrey Shmigelskiy <shmigelskiyandrey@gmail.com>
2024-09-09 22:13:58 +0400

    added css rotation, coloring, shapes

--- src/components/wheel.js
+++ src/components/wheel.js
@@ -2,9 +2,13 @@ import styles from "./wheel.module.css"
 
 function Wheel() {
     return (
-     <main className={styles.wheel}>
-      
-     </main>
+      <ul className={styles.wheel}>
+        <li className={styles.elem}>one</li>
+        <li className={styles.elem}>two</li>
+        <li className={styles.elem}>three</li>
+        <li className={styles.elem}>four</li>
+        <li className={styles.elem}>five</li>
+      </ul>
     );
   }
   
