cd0474555851c4c3512e6b9141652d06b1052d8b
Andrey Shmigelskiy <shmigelskiyandrey@gmail.com>
2024-09-10 17:31:20 +0400

    made require css appearence

--- src/components/wheel.js
+++ src/components/wheel.js
@@ -8,6 +8,13 @@ function Wheel() {
         <li className={styles.elem}>three</li>
         <li className={styles.elem}>four</li>
         <li className={styles.elem}>five</li>
+        <li className={styles.elem}>six</li>
+        <li className={styles.elem}>seven</li>
+        <li className={styles.elem}>eight</li>
+        <li className={styles.elem}>nine</li>
+        <li className={styles.elem}>ten</li>
+        <li className={styles.elem}>eleven</li>
+        <li className={styles.elem}>twelve</li>
       </ul>
     );
   }
