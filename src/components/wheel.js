bcab32f7f4283f98fa732916a5f4767fba7706f4
Andrey Shmigelskiy <shmigelskiyandrey@gmail.com>
2024-09-10 19:54:54 +0400

    tryed another way to make css appearence using conic gradient

--- src/components/wheel.js
+++ src/components/wheel.js
@@ -2,20 +2,21 @@ import styles from "./wheel.module.css"
 
 function Wheel() {
     return (
-      <ul className={styles.wheel}>
-        <li className={styles.elem}>one</li>
-        <li className={styles.elem}>two</li>
-        <li className={styles.elem}>three</li>
-        <li className={styles.elem}>four</li>
-        <li className={styles.elem}>five</li>
-        <li className={styles.elem}>six</li>
-        <li className={styles.elem}>seven</li>
-        <li className={styles.elem}>eight</li>
-        <li className={styles.elem}>nine</li>
-        <li className={styles.elem}>ten</li>
-        <li className={styles.elem}>eleven</li>
-        <li className={styles.elem}>twelve</li>
-      </ul>
+      // <ul className={styles.wheel}>
+      //   <li className={styles.elem}>one</li>
+      //   <li className={styles.elem}>two</li>
+      //   <li className={styles.elem}>three</li>
+      //   <li className={styles.elem}>four</li>
+      //   <li className={styles.elem}>five</li>
+      //   <li className={styles.elem}>six</li>
+      //   <li className={styles.elem}>seven</li>
+      //   <li className={styles.elem}>eight</li>
+      //   <li className={styles.elem}>nine</li>
+      //   <li className={styles.elem}>ten</li>
+      //   <li className={styles.elem}>eleven</li>
+      //   <li className={styles.elem}>twelve</li>
+      // </ul>
+      <div className={styles.dodo}>sas</div>
     );
   }
   
