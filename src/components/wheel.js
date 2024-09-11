dc29f475f615cb2f804eacca3dafdd9d0e16523a
Andrey Shmigelskiy <shmigelskiyandrey@gmail.com>
2024-09-11 20:51:16 +0400

    creating functianality to pass values to wheel

--- src/components/wheel.js
+++ src/components/wheel.js
@@ -1,23 +1,34 @@
 import styles from "./wheel.module.css"
 
+let arr = ["one", "two", "three", "four"]
+
 function Wheel() {
+  const list = document.getElementById("da")
+
+ if (document.readyState === 'complete') {
+  console.log("yyy")
+  arr.map((elem)=>(
+    list.append(document.createElement("div"))
+  ))
+}
+  
     return (
-      // <ul className={styles.wheel}>
-      //   <li className={styles.elem}>one</li>
-      //   <li className={styles.elem}>two</li>
-      //   <li className={styles.elem}>three</li>
-      //   <li className={styles.elem}>four</li>
-      //   <li className={styles.elem}>five</li>
-      //   <li className={styles.elem}>six</li>
-      //   <li className={styles.elem}>seven</li>
-      //   <li className={styles.elem}>eight</li>
-      //   <li className={styles.elem}>nine</li>
-      //   <li className={styles.elem}>ten</li>
-      //   <li className={styles.elem}>eleven</li>
-      //   <li className={styles.elem}>twelve</li>
-      // </ul>
-      <div className={styles.dodo}>sas</div>
+      <ul className={styles.wheel} id="da">
+        {/* <li className={styles.elem}>one</li>
+        <li className={styles.elem}>two</li>
+        <li className={styles.elem}>three</li>
+        <li className={styles.elem}>four</li>
+        <li className={styles.elem}>five</li>
+        <li className={styles.elem}>six</li>
+        <li className={styles.elem}>seven</li>
+        <li className={styles.elem}>eight</li>
+        <li className={styles.elem}>nine</li>
+        <li className={styles.elem}>ten</li>
+        <li className={styles.elem}>eleven</li>
+        <li className={styles.elem}>twelve</li> */}
+      </ul>
     );
+    
   }
   
   export { Wheel }
