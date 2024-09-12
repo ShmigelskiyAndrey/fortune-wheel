20aefb4a55c22ff3dc1c0ed1a03cccfa35bf7375
Andrey Shmigelskiy <shmigelskiyandrey@gmail.com>
2024-09-12 22:48:10 +0400

    updated elements generation

--- src/components/wheel.js
+++ src/components/wheel.js
@@ -1,29 +1,18 @@
 import styles from "./wheel.module.css"
 
 function Wheel() {
-  const arr = ["one", "two", "three", "four"]
+  const arr = ["one", "two", "three", "four", "five", "six"]
+  const quantity = arr.length
 
   const generateElems = ()=> {
-    const elem = arr.map((text)=>(<li className={styles.elem}>{text}</li>))
+    const elem = arr.map((elem, index)=>(<li className={styles.elem} style={{"--idx": index}} key={index}>{elem}</li>))
     return <ul className={styles.wheel}>{elem}</ul>
   }
-   
-    return (
-      <div className={styles.container}>
-        {generateElems()}
 
-        {/* <li className={styles.elem}>one</li>
-        <li className={styles.elem}>two</li>
-        <li className={styles.elem}>three</li>
-        <li className={styles.elem}>four</li>
-        <li className={styles.elem}>five</li>
-        <li className={styles.elem}>six</li>
-        <li className={styles.elem}>seven</li>
-        <li className={styles.elem}>eight</li>
-        <li className={styles.elem}>nine</li>
-        <li className={styles.elem}>ten</li>
-        <li className={styles.elem}>eleven</li>
-        <li className={styles.elem}>twelve</li> */}
+
+    return (
+      <div className={styles.container} style={{"--quantity": quantity}}>
+        {generateElems()}
       </div>
     );
     
