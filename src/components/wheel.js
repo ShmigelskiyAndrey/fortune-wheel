d2257ea6bdfcd9a8c707e54db4a0070d167e8786
Andrey Shmigelskiy <shmigelskiyandrey@gmail.com>
2024-09-13 21:18:12 +0400

    added spin button and rotation animation

--- src/components/wheel.js
+++ src/components/wheel.js
@@ -4,15 +4,37 @@ function Wheel() {
   const arr = ["one", "two", "three", "four", "five", "six"]
   const quantity = arr.length
 
-  const generateElems = ()=> {
+  const generateElems = () => {
     const elem = arr.map((elem, index)=>(<li className={styles.elem} style={{"--idx": index}} key={index}>{elem}</li>))
-    return <ul className={styles.wheel}>{elem}</ul>
+    return <ul className={styles.wheel} id="wheel">{elem}</ul>
   }
 
+  const rotation = () => {
+    const wheel = document.getElementById("wheel");
+    const rotationDegree = (Math.random() * 360) + 1800;
+
+    let animation;
+    let previousRotationDegree = 0;
+    const newRotationDegree = previousRotationDegree + rotationDegree;
+
+    animation = wheel.animate([
+      { transform: `rotate(${previousRotationDegree}deg)` },
+      { transform: `rotate(${newRotationDegree}deg)` }
+    ], {
+      duration: 4000,
+      direction: 'normal',
+      easing: 'cubic-bezier(0.440, -0.205, 0.000, 1.130)',
+      fill: 'forwards',
+      iterations: 1
+    });
+
+    console.log(animation)
+  }
 
     return (
       <div className={styles.container} style={{"--quantity": quantity}}>
         {generateElems()}
+        <button className={styles.spin} onClick={()=> {rotation()}}>Spin</button>
       </div>
     );
     
