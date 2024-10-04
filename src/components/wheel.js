778751d48d9e939d0df9742c881919d73bfb34e9
Andrey Shmigelskiy <shmigelskiyandrey@gmail.com>
2024-10-04 21:30:23 +0400

    upgraded rotation function
    Co-authored-by: Artem Iakovlev <iakovlev.artem.a@gmail.com>

--- src/components/wheel.js
+++ src/components/wheel.js
@@ -1,40 +1,11 @@
+import { useEffect, useCallback } from "react"
 import styles from "./wheel.module.css"
 
-function Wheel() {
-  const arr = ["one", "two", "three", "four", "five"]
+function Wheel({size = window.innerWidth / 100 * 50}) {
+  const arr = ["one", "two", "еркуу"]
   const quantity = arr.length
 
-  const generateElems = () => {
-    const elem = arr.map((elem, index)=>(<li className={styles.elem} style={{"--idx": index}} key={index}><span style={{"--position": index}}>{elem}</span></li>))
-    return <ul className={styles.wheel} id="wheel" style={{"--size": "600px"}}>{elem}</ul>
-  }
-
-  const rotation = () => {
-    const wheel = document.getElementById("wheel");
-    const spinBtn = document.getElementById("spinBtn") 
-    let previousRotationDegree = 0;
-
-    spinBtn.addEventListener('click', () => {
-      const rotationDegree = (Math.random() * 360) + 1800;
-      const newRotationDegree = previousRotationDegree + rotationDegree;
-      const realRotationDegree = newRotationDegree - 1800;
-
-      wheel.animate([
-        { transform: `rotate(${previousRotationDegree}deg)` },
-        { transform: `rotate(${newRotationDegree}deg)` }
-      ], {
-        duration: 4000,
-        direction: 'normal',
-        easing: 'cubic-bezier(0.440, -0.205, 0.000, 1.130)',
-        fill: 'forwards',
-        iterations: 1
-      });
-      previousRotationDegree = realRotationDegree;
-      result(realRotationDegree);
-    })
-  }
-
-  const result = (realRotationDegree) => {
+  const result = useCallback((realRotationDegree) => {
     const degreePerElement = 360 / quantity;
     let choosenElement;
 
@@ -60,10 +31,38 @@ function Wheel() {
       duration: 1000,
       easing: 'cubic-bezier(0.12, 0, 0.39, 0)',
     })
+  },[quantity])
+
+  useEffect(()=>{
+    const wheel = document.getElementById("wheel");
+    const spinBtn = document.getElementById("spinBtn") 
+    let previousRotationDegree = 0;
+    
+    spinBtn.addEventListener('click', () => {
+      const rotationDegree = (Math.random() * 360) + 1800;
+      const newRotationDegree = previousRotationDegree + rotationDegree;
+      const realRotationDegree = newRotationDegree - 1800;
+
+      wheel.animate([
+        { transform: `rotate(${previousRotationDegree}deg)` },
+        { transform: `rotate(${newRotationDegree}deg)` }
+      ], {
+        duration: 4000,
+        direction: 'normal',
+        easing: 'cubic-bezier(0.440, -0.205, 0.000, 1.130)',
+        fill: 'forwards',
+        iterations: 1
+      });
+      previousRotationDegree = realRotationDegree;
+      result(realRotationDegree);
+    })
+  },[result])
+
+  const generateElems = () => {
+    const elem = arr.map((elem, index)=>(<li className={styles.elem} style={{"--idx": index}} key={index}><span style={{"--position": index}}>{elem}</span></li>))
+    return <ul className={styles.wheel} id="wheel" style={{"--size": size + "px"}}>{elem}</ul>
   }
-
-  setTimeout(()=> {rotation()}, 1)
-
+  
     return (
       <div className={styles.container} style={{"--quantity": quantity}}>
         <div className={styles.picker}></div>
